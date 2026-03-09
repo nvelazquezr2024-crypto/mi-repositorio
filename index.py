27febef99b6ec78ee356869502e01864628ca224
nvelazquezr2024-crypto <nvelazquezr2024@gmail.com>
2026-03-08 23:05:05 -0600

    feat: Calculate and display the area of a circle based on user input.

--- index.py
+++ index.py
@@ -1,1 +1,6 @@
-print("hola mundo")
+import math
+radio = input("Ingrese el valor del radio del circulo: ")
+
+valor_radio = float(radio)
+area_circulo = (valor_radio ** 2) * 3.14159
+print(f"El area del circulo es: {area_circulo}")
